7b0d32153846c9bf962207bbdaca70a7e9a2def6
Zoe Pan <zoe.yi.pan@gmail.com>
2025-08-06 15:17:38 -0700

    Created UI View

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,6 +12,16 @@ struct ContentView: View {
         VStack {
             Text("What is your name?")
                 .font(.title)
+            TextField("Type your name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
